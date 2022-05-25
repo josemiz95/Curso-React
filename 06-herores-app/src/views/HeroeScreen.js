5c483989bd3c434057de91d09fc47432481fa8cf
Jose Miguel Ruiz Lopez <jose.ruiz@novafori.com>
2022-05-25 11:58:03 +0200

    Images form src

--- 06-herores-app/src/views/HeroeScreen.js
+++ 06-herores-app/src/views/HeroeScreen.js
@@ -2,6 +2,9 @@ import { useMemo } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroeById } from "../helpers/getHeroes";
 
+// import batman from "../assets/heroes/dc-batman.jpg"; // imagenes estaticas
+const heroImages = require.context('../assets/heroes', true);
+
 export const HeroeScreen = () => {
 	const {id: heroId} = useParams();
 	const navigate = useNavigate();
@@ -9,7 +12,7 @@ export const HeroeScreen = () => {
 	const hero = useMemo(() => getHeroeById(heroId), [heroId]);
 	if(!hero) return <Navigate to="/"/>
 
-	const imagePath = `/assets/heroes/${hero.id}.jpg`;
+	// const imagePath = `/assets/heroes/${hero.id}.jpg`;
 
 	const handleReturn = () => navigate(-1);
 
@@ -18,7 +21,13 @@ export const HeroeScreen = () => {
 		<div className="row mt-5">
 			<h1>{hero?.superhero}</h1>
 			<div className="col-md-4">
-				<img src={imagePath} alt={hero.superhero} className="img-thumbnail animate__animated animate__fadeInLeft"/>
+				{/* <img src={imagePath} alt={hero.superhero} className="img-thumbnail animate__animated animate__fadeInLeft"/> */}
+
+				{/* NOTE: Desde el archivo de assets en src */}
+				{/* <img src={batman} alt={hero.superhero} className="img-thumbnail animate__animated animate__fadeInLeft"/> */}
+
+				{/* NOTE: Obtener desde directorio */}
+				<img src={heroImages(`./${hero.id}.jpg`)} alt={hero.superhero} className="img-thumbnail animate__animated animate__fadeInLeft"/>
 			</div>
 			<div className="col-md-8 animate__animated animate__fadeInRight">
 				<h3>{hero.superhero}</h3>
