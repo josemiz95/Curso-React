76ff8c1e1d0b885fa8c84bb362fb632d57c018bc
Jose Miguel Ruiz Lopez <jose.ruiz@novafori.com>
2022-05-31 11:19:21 +0200

    sidebar

---
+++ 07-journal-app/src/components/journal/SideBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const SideBar = () => {
+	return (
+		<aside className='journal__sidebar'>
+			<div className='journal__sidebar-navbar'>
+				<h3 className='mt-1'><i className='far fa-moon'/><span>Josemi</span></h3>
+				<button className='btn'>Logout</button>
+			</div>
+
+			<div className='journal__new-entry'>
+				<i className='far fa-calendar-plus fa-3x'/>
+				<p className='mt-1'>New entry</p>
+			</div>
+		</aside>
+	)
+}
--- 07-journal-app/src/views/journal/Journal.js
+++ 07-journal-app/src/views/journal/Journal.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { SideBar } from '../../components/journal/SideBar'
 
 export const Journal = () => {
-  return (
-	<div>Journal</div>
-  )
+    return (
+		<div className='journal__main-content'>
+			<SideBar/>
+
+			<main>
+				<h1>Main content</h1>
+			</main>
+		</div>
+    )
 }
